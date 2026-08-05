ff42973a8318bdd1a29cf641219783f2416760ea
iftrue <iftrue@users.noreply.github.com>
2026-08-05 22:37:51 +0800

    chore: remove junk files (agent meta-docs, outdated installers, deprecated startup scripts)

--- start_router.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Model Router 自启动脚本
-REM 放入 shell:startup 或手动运行
-
-cd /d "%~dp0"
-python -m uvicorn model_router.app:app --host 127.0.0.1 --port 6060
-echo Model Router started (http://127.0.0.1:6060)
